85dda89c961e7cf09bb93cfb4ce0564673e64ea0
maicon <maiconavila00@gmail.com>
2020-01-21 23:01:47 -0300

    Updates

---
+++ precache-manifest.f20ab59dee41c98754e56c3f8e504c97.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e658a26c6a8f906e510d4c48b7123d4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fe26dc2ec17a3a19fcf",
+    "url": "/static/css/main.61fd84de.chunk.css"
+  },
+  {
+    "revision": "61a96230a05451bcb1a7",
+    "url": "/static/js/2.b72fb86f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.b72fb86f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fe26dc2ec17a3a19fcf",
+    "url": "/static/js/main.1ed3bd1f.chunk.js"
+  },
+  {
+    "revision": "48e2c5c1cd86bc39aab1",
+    "url": "/static/js/runtime-main.3817439c.js"
+  },
+  {
+    "revision": "459ac4d4f360c26578dbb2c9e493d377",
+    "url": "/static/media/capitao-fantastico.459ac4d4.svg"
+  },
+  {
+    "revision": "3b80531ca4e0c9cf127a7f0f2cea4234",
+    "url": "/static/media/claquete.3b80531c.svg"
+  }
+]);

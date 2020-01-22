5e964c43cc34b7cda8b0bb8cd8286b9529f1240f
maicon <maiconavila00@gmail.com>
2020-01-21 23:21:26 -0300

    Updates

---
+++ precache-manifest.de751bd4a57b0169df98c86efeaf6fe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1411bd1afd968f5d81aff370b1790df",
+    "url": "/ToDoFlix/index.html"
+  },
+  {
+    "revision": "2fe26dc2ec17a3a19fcf",
+    "url": "/ToDoFlix/static/css/main.61fd84de.chunk.css"
+  },
+  {
+    "revision": "b192737c3bd1ffc03a4c",
+    "url": "/ToDoFlix/static/js/2.fe04238c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ToDoFlix/static/js/2.fe04238c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2fe26dc2ec17a3a19fcf",
+    "url": "/ToDoFlix/static/js/main.1ed3bd1f.chunk.js"
+  },
+  {
+    "revision": "d94b5962aa505f26e751",
+    "url": "/ToDoFlix/static/js/runtime-main.ee991eea.js"
+  },
+  {
+    "revision": "459ac4d4f360c26578dbb2c9e493d377",
+    "url": "/ToDoFlix/static/media/capitao-fantastico.459ac4d4.svg"
+  },
+  {
+    "revision": "3b80531ca4e0c9cf127a7f0f2cea4234",
+    "url": "/ToDoFlix/static/media/claquete.3b80531c.svg"
+  }
+]);
